46a363b1a64f637d9281da90a6700af59b7178ae
Vladislav <timaevvladislav@gmail.com>
2023-02-28 01:59:28 +0600

    add:layouts

--- src/pages/components/Footer.tsx
+++ src/pages/components/Footer.tsx
@@ -8,7 +8,7 @@ export default function Footer() {
                         <h5 className="white-text">Company Bio</h5>
                         <p className="grey-text text-lighten-4">
                             We are a team of college students working on this
-                            project like our full time job. Any amount would help support and continue development
+                            project like iour full time job. Any amount would help support and continue development
                             on this project and is greatly appreciated.
                         </p>
                     </div>

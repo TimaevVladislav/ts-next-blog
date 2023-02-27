fbf217867e1b335c39f40bceab3a0281a4d954de
Vladislav <timaevvladislav@gmail.com>
2023-02-28 01:23:29 +0600

    add:head

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,14 @@
 import Link from "next/link"
+import Head from "next/head"
 
 export default function Home() {
   return (
     <>
+      <Head>
+          <title>Next blog</title>
+          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css" />
+
+      </Head>
       <h1>Home</h1>
       <Link href="/about">
           About

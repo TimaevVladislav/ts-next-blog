24a693dd089e0253386983a80ee779c85e74b036
Vladislav <timaevvladislav@gmail.com>
2023-02-28 01:07:01 +0600

    add:router

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,6 +1,17 @@
+import Router from "next/router"
 
 export default function Index() {
-    return(
-        <h1>About page</h1>
+
+    const linkClickHandler = () => {
+        Router.push("/")
+    }
+
+    return (
+        <>
+           <h1>About page</h1>
+
+           <button onClick={linkClickHandler}>Go back to home</button>
+           <button onClick={() => Router.push("/posts")}>Go to posts</button>
+        </>
     )
 }

bae87ffdacdbab9e6a637dabcabd9b4773d9ced0
Vladislav <timaevvladislav@gmail.com>
2023-02-28 14:39:09 +0600

    init

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,10 +1,11 @@
 import Link from "next/link"
+import classes from "../styles/Error.module.css"
 
 export default function ErrorPage() {
     return (
-        <>
-        <h1>Error page</h1>
-        <Link href="/">Go back to safety</Link>
-        </>
+        <div className="container">
+            <h1 className={classes.error}>Error page</h1>
+            <Link href="/">Go back to safety</Link>
+        </div>
     )
 }
--- src/pages/models/model.ts
+++ src/pages/models/model.ts
@@ -4,3 +4,9 @@ export interface INavigationProps {
    children: any
    title: string
 }
+
+export interface IPosts {
+   id: number
+   title: string
+   body: string
+}

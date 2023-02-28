7ff091d24aeaf6e8b5693dddd70ca40e669a954f
Vladislav <timaevvladislav@gmail.com>
2023-02-28 15:46:24 +0600

    add:server:post:rendering

--- src/pages/post/[id].tsx
+++ src/pages/post/[id].tsx
@@ -1,14 +1,49 @@
-import {useRouter} from "next/router"
+import {useState, useEffect} from "react"
 import Navigation from "@/pages/components/Navigation"
+import {useRouter} from "next/router"
 
-export default function Post() {
+export default function Post({ post: serverPost }: any) {
+    const [post, setPost] = useState(serverPost)
     const router = useRouter()
 
+    useEffect(() => {
+        async function load() {
+            const response = await fetch(`http://localhost:4200/posts/${router.query.id}`)
+            const data = await response.json()
+
+            setPost(data)
+        }
+
+        if (!serverPost) {
+            load()
+        }
+    }, [])
+
+    if (!post) {
+        return (
+            <Navigation title="Loading">
+                <p>Loading...</p>
+            </Navigation>
+        )
+    }
+
     return (
-        <Navigation title={"Post"}>
-            <h1>Post {router.query.id}</h1>
-            <h2>{router.query.title}</h2>
-            <p>{router.query.body}</p>
+        <Navigation title={`${post.title}`}>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
         </Navigation>
     )
+}
+
+
+Post.getInitialProps = async ({ query, req }: any) => {
+
+    if (!req) {
+        return { post: null }
+    }
+
+    const response = await fetch(`http://localhost:4200/posts/${query.id}`)
+    const post = await response.json()
+
+    return { post }
 }
--- src/pages/posts.tsx
+++ src/pages/posts.tsx
@@ -1,18 +1,39 @@
 import Navigation from "@/pages/components/Navigation"
+import {useEffect, useState} from "react"
 import {IPosts} from "@/pages/models/model"
 import Link from "next/link"
 
-export default function Posts({ posts }: any) {
+export default function Posts({ posts: serverPosts }: any) {
+    const [posts, setPosts] = useState(serverPosts)
 
+    useEffect(() => {
+        async function load() {
+            const response = await fetch(`http://localhost:4200/posts`)
+            const data = await response.json()
 
+            setPosts(data)
+        }
 
-    return(
+        if (!serverPosts) {
+            load()
+        }
+    }, [])
+
+    if (!posts) {
+        return (
+            <Navigation title="Loading">
+                <p>Loading...</p>
+            </Navigation>
+        )
+    }
+
+    return (
         <Navigation title="Posts page">
             <h1>Posts</h1>
             <ul>
                 {posts.map((post: IPosts)=> (
                     <li key={post.id}>
-                        <Link href={{pathname: `/post/[id]`, query: { ...post }}} as={`/post/${post.id}`}>
+                        <Link href={{pathname: `/post/[id]`}} as={`/post/${post.id}`}>
                             {post.title}
                         </Link>
                     </li>
@@ -22,8 +43,13 @@ export default function Posts({ posts }: any) {
     )
 }
 
-Posts.getInitialProps = async () => {
-    const response = await fetch("http://localhost:4200/posts")
+Posts.getInitialProps = async ({ req}: any) => {
+
+    if (!req) {
+        return { posts: null }
+    }
+
+    const response = await fetch(`http://localhost:4200/posts`)
     const posts = await response.json()
 
     return { posts }
